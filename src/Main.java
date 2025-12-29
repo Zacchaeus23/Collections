d2b0730043b7d6bcc93d2a4ff2f13d9d32f8185b
Michael Zacchaeus <108746426+Zacchaeus23@users.noreply.github.com>
2025-12-29 12:52:59 +0100

    Using generics with collection

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ import java.util.Collection;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Collection values = new ArrayList();
+        Collection<Integer> values = new ArrayList<>();
         values.add(1);
-        values.add("John");
 
         System.out.println(values);
     }
