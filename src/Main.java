f55df9c3c48ed924a11ebf713de0247721a1e99b
Michael Zacchaeus <108746426+Zacchaeus23@users.noreply.github.com>
2025-12-29 12:46:15 +0100

    Collection interface

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Collection values = new ArrayList();
+        values.add(1);
+        values.add("John");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(values);
     }
 }
