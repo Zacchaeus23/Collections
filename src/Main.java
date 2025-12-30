0b40db3bc19e7e001481f641e2275fed0d02b73a
Michael Zacchaeus <108746426+Zacchaeus23@users.noreply.github.com>
2025-12-30 02:24:45 +0100

    List and adding an integer to the list with the index number

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<Integer> values = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
         values.add(3);
         values.add(77);
         values.add(88);
 
-//        Iterator i = values.iterator();
-//        while (i.hasNext())
-//            System.out.println(i.next());
+        values.add(1,9);
 
         for (int i : values)
             System.out.println(i);
